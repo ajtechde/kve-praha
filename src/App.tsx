48ac915aa6e82922bfbe0e483ae903a9d38b28b9
Alexander Jakovec <alexanderjakovec@gmail.com>
2026-08-22 22:47:08 +0200

    Add ScrollToTop on route change

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,16 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { LanguageProvider } from "@/i18n/LanguageContext";
+import { useEffect } from "react";
 import Index from "./pages/Index";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => { window.scrollTo(0, 0); }, [pathname]);
+  return null;
+}
 import About from "./pages/About";
 import Beliefs from "./pages/Beliefs";
 import Locations from "./pages/Locations";
@@ -20,6 +27,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <LanguageProvider>
+          <ScrollToTop />
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/about" element={<About />} />
